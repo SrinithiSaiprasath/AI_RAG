484cc33b6991d45486b5d48f61cf4d24e5ec61df
SrinithiSaiprasath <srinithisaiprasathg@gmail.com>
2025-05-28 22:53:26 +0530

    c

---
+++ Admin/admin.py
@@ -0,0 +1,93 @@
+import boto3
+import streamlit as st
+import os
+import uuid
+
+## s3_client
+s3_client = boto3.client("s3")
+# BUCKET_NAME = os.getenv("BUCKET_NAME")
+BUCKET_NAME = os.getenv("BUCKET_NAME", "admin-chatpdf")
+AWS_REGION = os.getenv("AWS_REGION", "eu-north-1")
+
+## Bedrock
+# from langchain_community.embeddings import BedrockEmbeddings
+from langchain_aws.embeddings import BedrockEmbeddings
+## Text Splitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+## Pdf Loader
+from langchain_community.document_loaders import PyPDFLoader
+
+## import FAISS
+from langchain_community.vectorstores import FAISS
+
+bedrock_client = boto3.client(service_name="bedrock-runtime" , region_name= "eu-north-1")
+bedrock_embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v2:0", client=bedrock_client)
+
+def get_unique_id():
+    return str(uuid.uuid4())
+
+
+## Split the pages / text into chunks
+def split_text(pages, chunk_size, chunk_overlap):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    docs = text_splitter.split_documents(pages)
+    return docs
+
+## create vector store
+def create_vector_store(request_id, documents):
+    vectorstore_faiss=FAISS.from_documents(documents, bedrock_embeddings)
+    file_name=f"{request_id}.bin"
+    folder_path="/tmp/"
+    vectorstore_faiss.save_local(index_name=file_name, folder_path=folder_path)
+
+    ## upload to S3
+    # s3_client.upload_file(Filename=folder_path + "/" + file_name + ".faiss", Bucket=os.getenv("BUCKET_NAME"), Key="my_faiss.faiss")
+    # s3_client.upload_file(Filename=folder_path + "/" + file_name + ".pkl", Bucket=os.getenv("BUCKET_NAME"), Key="my_faiss.pkl")
+
+    s3_client.upload_file(Filename=os.path.join(folder_path, file_name + ".faiss"), Bucket=BUCKET_NAME, Key="my_faiss.faiss")
+    s3_client.upload_file(Filename=os.path.join(folder_path, file_name + ".pkl"), Bucket=BUCKET_NAME, Key="my_faiss.pkl")
+    st.write("Vector store created and uploaded to S3 successfully.")
+    st.write(f"Vector store file name: {file_name}")
+
+    return True
+
+## main method
+def main():
+    st.write("This is Admin Site for Chat with PDF demo")
+    uploaded_file = st.file_uploader("Choose a file", type = ["pdf","csv","txt","docx"] , allow_multiple_files=True)
+    if uploaded_file is not None:
+        request_id = get_unique_id()
+        st.write(f"Request Id: {request_id}")
+        saved_file_name = f"{request_id}.pdf"
+        with open(saved_file_name, mode="wb") as w:
+            w.write(uploaded_file.getvalue())
+
+        loader = PyPDFLoader(saved_file_name)
+        pages = loader.load_and_split()
+
+        st.write(f"Total Pages: {len(pages)}")
+
+        progress = st.progress(0, text="Starting vector store creation...")
+
+        progress.progress(10, "Splitting text...")
+        splitted_docs = split_text(pages, 1000, 200)
+        st.write(f"Splitted Docs length: {len(splitted_docs)}")
+
+        progress.progress(40, "Creating vector embeddings...")
+        progress.progress(70, "Saving vector store and uploading to S3...")
+        result = create_vector_store(request_id, splitted_docs)
+
+        progress.progress(100, "Completed!")
+
+        if result:
+            st.success("✅ Hurray!! PDF processed successfully")
+        else:
+            st.error("❌ Error!! Please check logs.")
+        os.remove(saved_file_name)
+        st.balloons()
+        st.write("You can now go to the chat page to interact with the PDF.")
+        st.write("Go to [Chat Page](http://localhost:8501/chat)")
+
+if __name__ == "__main__":
+    main()
---
+++ User/app.py
@@ -0,0 +1,119 @@
+import boto3
+import streamlit as st
+import os
+import uuid
+import dotenv
+dotenv.load_dotenv()
+
+## s3_client
+s3_client = boto3.client("s3")
+BUCKET_NAME = os.getenv("BUCKET_NAME", "admin-chatpdf")
+AWS_REGION = os.getenv("AWS_REGION", "eu-north-1")
+
+## Bedrock
+# from langchain_community.embeddings import BedrockEmbeddings
+# from langchain.llms.bedrock import Bedrock
+## prompt and chain
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain_community.llms import Bedrock
+from langchain_aws.embeddings import BedrockEmbeddings
+from langchain_aws.llms import BedrockLLM 
+## Text Splitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+## Pdf Loader
+from langchain_community.document_loaders import PyPDFLoader
+
+## import FAISS
+from langchain_community.vectorstores import FAISS
+
+bedrock_client = boto3.client(service_name="bedrock-runtime")
+bedrock_embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v2:0", client=bedrock_client)
+
+folder_path="/tmp/"
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+def get_unique_id():
+    return str(uuid.uuid4())
+
+## load index
+def load_index():
+    s3_client.download_file(Bucket=BUCKET_NAME, Key="my_faiss.faiss", Filename=f"{folder_path}my_faiss.faiss")
+    s3_client.download_file(Bucket=BUCKET_NAME, Key="my_faiss.pkl", Filename=f"{folder_path}my_faiss.pkl")
+
+def get_llm():
+    llm = BedrockLLM(
+        model_id="mistral.mixtral-8x7b-instruct-v0:1",  # ✅ valid model ID
+        client=bedrock_client,
+        model_kwargs={'max_tokens': 512}
+    )
+    return llm
+
+# get_response()
+def get_response(llm,vectorstore, question ):
+    ## create prompt / template
+    prompt_template = """
+
+    Human: Please use the given context to provide concise answer to the question
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    <context>
+    {context}
+    </context>
+
+    Question: {question}
+
+    Assistant:"""
+
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+
+    qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vectorstore.as_retriever(
+        search_type="mmr", search_kwargs={"k": 5 , "lambda_mult": 0.5}
+    ),
+    return_source_documents=True,
+    chain_type_kwargs={"prompt": PROMPT}
+)
+    answer=qa({"query":question})
+    return answer['result']
+
+
+## main method
+def main():
+    st.header("This is Client Site for Chat with PDF demo using Bedrock, RAG etc")
+
+    load_index()
+
+    dir_list = os.listdir(folder_path)
+    # st.write(f"Files and Directories in {folder_path}")
+    # st.write(dir_list)
+
+    ## create index
+    faiss_index = FAISS.load_local(
+        index_name="my_faiss",
+        folder_path = folder_path,
+        embeddings=bedrock_embeddings,
+        allow_dangerous_deserialization=True
+    )
+
+    st.write("MODEL INDEXED FOR RETRIVAL")
+    question = st.text_input("Please ask your question")
+    if st.button("Ask Question"):
+        with st.spinner("Querying..."):
+
+            llm = get_llm()
+
+            # get_response
+            st.write(get_response(llm, faiss_index, question))
+            st.success("Done")
+            st.balloons()
+            st.write("You can ask another question or upload a new PDF file.")
+            st.write("Go to [Admin Page](http://localhost:8501/admin) to upload a new PDF file.")
+    
+
+if __name__ == "__main__":
+    main()
